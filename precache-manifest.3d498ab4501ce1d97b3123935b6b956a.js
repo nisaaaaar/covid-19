a982c219c527a98d28c0e7d803a05c7b925614d0
Nisar <nisara305@gmail.com>
2020-08-06 14:02:19 -0500

    Updates

---
+++ precache-manifest.3d498ab4501ce1d97b3123935b6b956a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3446adfde7c73101729c04193703b344",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "688f8e78fe659dcaf317",
+    "url": "/covid-19/static/css/2.e307d2d6.chunk.css"
+  },
+  {
+    "revision": "0d911ae7391e18cafd9d",
+    "url": "/covid-19/static/css/main.41c8e17d.chunk.css"
+  },
+  {
+    "revision": "688f8e78fe659dcaf317",
+    "url": "/covid-19/static/js/2.d8c1b85e.chunk.js"
+  },
+  {
+    "revision": "6d94984a5fdbad217098935a3d45084e",
+    "url": "/covid-19/static/js/2.d8c1b85e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d911ae7391e18cafd9d",
+    "url": "/covid-19/static/js/main.95c9bc55.chunk.js"
+  },
+  {
+    "revision": "e84dd164dd3b06d1310b",
+    "url": "/covid-19/static/js/runtime-main.6ca52c11.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/covid-19/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/covid-19/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/covid-19/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/covid-19/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/covid-19/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
